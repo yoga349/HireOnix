d78a8ad7afea7b54e6e457e694aa0337313c17ef
yoga349 <yp14115@gmail.com>
2026-07-31 10:58:40 +0530

    added candidate only Middleware

--- Backend/Middleware/roleMiddleware.js
+++ Backend/Middleware/roleMiddleware.js
@@ -8,3 +8,13 @@ export const recruiterOnly = (req, res, next) => {
 
   next();
 };
+
+export const candidateOnly = (req, res, next) => {
+    if (req.user.role !== "candidate") {
+        return res.status(403).json({
+            message: "Access denied",
+        });
+    }
+
+    next();
+};
