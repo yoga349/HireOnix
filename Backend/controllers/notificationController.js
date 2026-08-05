9aec9eb2ceba144fe5d3d1e55b436fc7d8a00703
yoga349 <yp14115@gmail.com>
2026-08-05 11:45:46 +0530

    updated notification controller

--- Backend/controllers/notificationController.js
+++ Backend/controllers/notificationController.js
@@ -26,25 +26,36 @@ export const getNotifications = async (req, res) => {
 export const markAsRead = async (req, res) => {
   try {
 
-    const notification =
-      await Notification.findByIdAndUpdate(
-        req.params.id,
-        {
-          isRead: true,
-        },
-        {
-          new: true,
-        }
-      );
+    const notification = await Notification.findOneAndUpdate(
+      {
+        _id: req.params.id,
+        user: req.user._id,
+      },
+      {
+        isRead: true,
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!notification) {
+      return res.status(404).json({
+        success: false,
+        message: "Notification not found",
+      });
+    }
 
     res.status(200).json({
       success: true,
+      message: "Notification marked as read",
       notification,
     });
 
   } catch (error) {
 
     res.status(500).json({
+      success: false,
       message: error.message,
     });
 
