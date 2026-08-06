fbf364cc49c4cc7234f199b6547d81865e604946
yoga349 <yp14115@gmail.com>
2026-08-07 00:18:45 +0530

    updated mail.js

--- Backend/config/mail.js
+++ Backend/config/mail.js
@@ -2,8 +2,6 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("EMAIL:", process.env.EMAIL);
-console.log("APP_PASSWORD:", process.env.APP_PASSWORD);
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
