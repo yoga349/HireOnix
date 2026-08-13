dc5b59d5bfab9fa1d1f7553cd89f57ba47b605ec
yoga349 <yp14115@gmail.com>
2026-08-13 16:57:54 +0530

    Added validation Middleware

---
+++ Backend/Middleware/validationMiddleware.js
@@ -0,0 +1,18 @@
+export const validate = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: "Validation failed",
+        errors: error.details.map((detail) => detail.message),
+      });
+    }
+
+    next();
+  };
+};
