008498d820135e89f58e8d020ceac3297a221419
yoga349 <yp14115@gmail.com>
2026-08-13 16:59:24 +0530

    updated auth route for validation

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { registerUser } from "../controllers/user.js";
 import { loginUser } from "../controllers/loginController.js";
+import { validate } from "../middleware/validationMiddleware.js";
+import {
+  registerValidator,
+  loginValidator,
+} from "../validators/userValidator.js";
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login",loginUser);
+router.post("/register", validate(registerValidator), registerUser);
+router.post("/login",validate(loginValidator),loginUser);
 export default router;
